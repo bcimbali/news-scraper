17822dfa76a4a246c53a1ad92ea9d45d6e7d6894
Brett Cimbalik <bcimbali@gmail.com>
2018-06-24 14:02:11 -0500

    saved articles now works

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get("/scrape", function(req, res) {
         });
     });
 
-    // If we were able to successfully scrape and save an Article, send a message to the client
+    // If we were able to successfully scrape and save an Article, redirect to home
     res.redirect("/");
   });
 });
@@ -99,14 +99,14 @@ app.get("/articles", function(req, res) {
     });
 });
 
-// Route for getting all Articles from the db
-app.get("/saved", function(req, res) {
+// Route for getting all Articles from the db - API/JSON route
+app.get("/articles/saved", function(req, res) {
   // Grab every document in the Articles collection
   db.Article.find({saved: true})
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
-      // res.json(dbArticle);
-      res.render('index');
+      res.json(dbArticle);
+      // res.json('root');
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
